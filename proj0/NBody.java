b68ddc8334a099897e4859606ad18544d2ee8f7e
ShadowAzrael <909456873@qq.com>
2022-12-30 00:47:18 +0800

    proj0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,75 +1,73 @@
 public class NBody {
-    public static double readRadius(String fileName) {
-        In in = new In(fileName);
-        in.readInt();  // Skip the number of planets
-        double radius = in.readDouble();
-        return radius;
-    }
 
-    public static Body[] readBodies(String fileName) {
-        In in = new In(fileName);
-        int numOfBodies = in.readInt();
-        Body[] bodies = new Body[numOfBodies];
-        in.readDouble();  // Skip the value of radius
-        for (int i = 0; i < numOfBodies; i++) {
-            bodies[i] = new Body(in.readDouble(), in.readDouble(), in.readDouble(),
-                    in.readDouble(), in.readDouble(), in.readString());
-        }
-        return bodies;
-    }
+	public static double readRadius(String filename){
+		In in = new In(filename);
+		int num = in.readInt();
+		double radius = in.readDouble();
+		return radius;
+	}
 
-    public static void main(String[] args) {
-        /* Parse arguments */
-        double T = Double.parseDouble(args[0]);
-        double dt = Double.parseDouble(args[1]);
-        String fileName = args[2];
+	public static Planet[] readPlanets(String filename){
+		In in = new In(filename);
+		int num = in.readInt();
+		double radius = in.readDouble();
+		Planet[] planets = new Planet[num];
+		for (int i = 0; i < num; i++) {
+			double xp = in.readDouble();
+			double yp = in.readDouble();
+			double vx = in.readDouble();
+			double vy = in.readDouble();
+			double m = in.readDouble();
+			String img = in.readString();
+			planets[i] = new Planet(xp, yp, vx, vy, m, img);	
+		}
+		return planets;
+	}
 
-        /* Read in data */
-        double radius = readRadius(fileName);
-        Body[] bodies = readBodies(fileName);
+	public static void main(String[] args) {
+		double T = new Double(args[0]);
+		double dt = new Double(args[1]);	
+		String filename = args[2];
+		double r = readRadius(filename);
+		Planet[] planets = readPlanets(filename);
 
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setScale(-radius, radius);
+		// set the universe scale
+		StdDraw.setXscale(-r, r);
+		StdDraw.setYscale(-r, r);
+		StdDraw.enableDoubleBuffering();
 
-        Double[] xForces = new Double[bodies.length];
-        Double[] yForces = new Double[bodies.length];
+		double t = 0;
+		int num = planets.length;
+		while(t <= T){
+			double[] xForces = new double[num];
+			double[] yForces = new double[num];
+			for(int i = 0; i < num; i++){
+				xForces[i] = planets[i].calcNetForceExertedByX(planets);
+				yForces[i] = planets[i].calcNetForceExertedByY(planets);
+			}
+			for(int i = 0; i < num; i++){
+				planets[i].update(dt, xForces[i], yForces[i]);
+			}
 
-        /* Play the theme to 2001: A Space Odyssey */
-        StdAudio.play("audio/2001.mid");
+			// draw the backgroud picture
+			StdDraw.picture(0, 0, "images/starfield.jpg");
 
-        Double currentTime = 0.0;
-        while (currentTime < T) {
-            /* Calculate the net forces for each Body */
-            for (int i = 0; i < bodies.length; i++) {
-                xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
-                yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
-            }
-            /* Update each Body’s position, velocity, and acceleration */
-            for (int i = 0; i < bodies.length; i++) {
-                bodies[i].update(dt, xForces[i], yForces[i]);
-            }
+			// draw all the planets
+			for (Planet planet : planets) {
+				planet.draw();	
+			}
 
-            StdDraw.clear();
-            /* Draw the background */
-            String background = "images/starfield.jpg";
-            StdDraw.picture(0, 0, background);
-            /* Draw all the bodies */
-            for (Body b : bodies) {
-                b.draw();
-            }
-            StdDraw.show();
-            StdDraw.pause(10);
+			StdDraw.show();
+			StdDraw.pause(10);
+			t += dt;
+		}
 
-            currentTime += dt;
-        }
-
-        /* Print the universe */
-        StdOut.printf("%d\n", bodies.length);
-        StdOut.printf("%.2e\n", radius);
-        for (int i = 0; i < bodies.length; i++) {
-            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                          bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
-                          bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);
-        }
-    }
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", r);
+		for (int i = 0; i < planets.length; i++) {
+    		StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+            planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
+            planets[i].yyVel, planets[i].mass, planets[i].imgFileName);   
+		}
+	}
 }
