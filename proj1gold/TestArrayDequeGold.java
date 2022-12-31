69fae63cb2b35f581f9c95e592cb862c6136a8ca
ShadowAzrael <909456873@qq.com>
2022-12-31 19:26:51 +0800

    lab3 updated

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+    }
+}
